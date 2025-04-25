ed696f634948f23cbe9bc5aca4d4367d9e317333
GeraldJones <gajone12@asu.edu>
2025-04-24 17:34:11 -0700

    Created a functional graph using 3D Vectors! y is the function that takes x. Then commiting those computations within the for loop. Really cool, but ugly.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -8,12 +8,16 @@ public class Graph : MonoBehaviour
     void Awake()
     {
         Transform point;
-        int i = 0;
-        while (i < 10)
+        var position = Vector3.zero; //Making position start as a zero 3d vector
+        var scale = Vector3.one / 5f; //Fixing scale to a 1 3D vector / 5f for proper cube size
+        
+        for (int i = 0; i < 10; i++)
         {
             point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right * i;
-            i++;
+            position.x = (i + 0.5f) / 5f - 1f; //Making the x component in position vector this location on scene
+            position.y = position.x * position.x; //Making y component a function, where you plug in x to get it's y coord
+            point.localPosition = position; //Setting cube position to the aforementioned
+            point.localScale = scale; //Setting scale to the initialized scale I did before the for loop
         }
     }
 }

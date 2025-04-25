38eedf5d4818e25ad3c1126058d27a2a2239c63b
GeraldJones <gajone12@asu.edu>
2025-04-24 17:00:13 -0700

    Added a Point prefab for code instantiation.

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] 
+    Transform pointPrefab;
+
+    void Awake()
+    {
+        Instantiate(pointPrefab);
+    }
+}

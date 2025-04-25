c563b067bc49a364c678ce0eccb14c6d71db3a19
GeraldJones <gajone12@asu.edu>
2025-04-24 18:02:13 -0700

    Now able to add a 'resolution' factor so graph is not lock to just 10 points.

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,20 +4,25 @@ public class Graph : MonoBehaviour
 {
     [SerializeField] 
     Transform pointPrefab;
+    
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
 
     void Awake()
     {
         Transform point;
+        float step = 2f / resolution;
         var position = Vector3.zero; //Making position start as a zero 3d vector
-        var scale = Vector3.one / 5f; //Fixing scale to a 1 3D vector / 5f for proper cube size
+        var scale = Vector3.one * step; //Fixing scale to a uniform 1 3D Vector to scale with resolution size
         
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < resolution; i++)
         {
             point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f; //Making the x component in position vector this location on scene
+            position.x = (i + 0.5f) * step - 1f; //Making the x component in position vector this location on scene
             position.y = position.x * position.x; //Making y component a function, where you plug in x to get it's y coord
             point.localPosition = position; //Setting cube position to the aforementioned
             point.localScale = scale; //Setting scale to the initialized scale I did before the for loop
         }
+        
     }
 }
